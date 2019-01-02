7e56cf8b9040066f9bf14e666b06f47a9ffb3a47
DanTsai0903 <46018332+DanTsai0903@users.noreply.github.com>
2019-01-02 21:35:32 +0800

    Update readdata.py
    完成stock class可以直接取用所有均線

--- readdata.py
+++ readdata.py
@@ -10,56 +10,66 @@ reader2 = csv.DictReader(filehandler)
 #csv.reader被csv.DictReader來取代，dictreader的好處是可以用欄位名稱來呼叫#
 ###################################################################
 cheader = reader2.fieldnames #這行的作用是不要管第一行（欄位名稱）
+print(cheader)
 
 
 
-stockDict = Dict() #儲存一家一家的公司，key是股票代碼，value是那間公司的「資料」（所以新建一個「stock」class, 這個class 儲存一家公司的資料）
-class stock():
-	def __init__(self):
+class stock:
+	def __init__(self,code):
 		#新建那些list, 包括date, opening, ......
-	def calculate(self):
-		#算出5日線，20日線 ......
-
-
+		#MA(moving average)均線
+		self.code = code
+		self.abbreviation = stockDict_alldata[code][0]#公司簡稱
+		self.opening = stockDict_alldata[code][2][-1]#今日開盤價
+		self.highest = stockDict_alldata[code][3][-1]#今日最高價
+		self.lowest =  stockDict_alldata[code][4][-1]#今日最低價
+		self.closing = stockDict_alldata[code][5][-1]#今日收盤價
+		self.volume = stockDict_alldata[code][6][-1]#今日成交量(千股)
+		self.MA5 = sum(stockDict_alldata[code][5][-5:-1])/5#周線(5日線)
+		self.MA20 = sum(stockDict_alldata[code][5][-20:-1])/20#月線(20日線)
+		self.MA60 = sum(stockDict_alldata[code][5][-60:-1])/60#季線(60日線)
+		
+	def __str__(self):
+		return self.code
+		
+	
+	
+stockDict_alldata = dict() #儲存一家一家的公司，key是股票代碼，value是那間公司的「歷史資料」
 for line in reader2:
 	#如果以「line["證券代碼"]」為key的資料不存在
-		#則新增一筆資料、key是line["證券代碼"]、value是stock()
+		#則新增一筆資料、key是line["證券代碼"]、value是歷史資料
 	#然後把這行line的每一欄位加入到以「line["證券代碼"]」為key的資料中
+	allapecificdata = []#一個大list，放下面所有小list
+	
+	date = []#時間
+	opening = []#開盤價
+	highest = []#最高價
+	lowest = []#最低價
+	closing = []#收盤價
+	volume = []#成交量
+
+	if line["證券代碼"].strip() not in stockDict_alldata.keys():
+		abbreviation = line["簡稱"].strip()
+		date.append(line["年月日"])
+		opening.append(float(line["開盤價(元)"]))
+		highest.append(float(line["最高價(元)"]))
+		lowest.append(float(line["最低價(元)"]))
+		closing.append(float(line["收盤價(元)"]))
+		volume.append(float(line["成交量(千股)"]))
+		allapecificdata = [abbreviation,date,opening,highest,lowest,closing,volume]
+		stockDict_alldata[line["證券代碼"].strip()] = allapecificdata
+		
+	else:
+		stockDict_alldata[line["證券代碼"].strip()][1].append(line["年月日"])
+		stockDict_alldata[line["證券代碼"].strip()][2].append(float(line["開盤價(元)"]))
+		stockDict_alldata[line["證券代碼"].strip()][3].append(float(line["最高價(元)"]))
+		stockDict_alldata[line["證券代碼"].strip()][4].append(float(line["最低價(元)"]))
+		stockDict_alldata[line["證券代碼"].strip()][5].append(float(line["收盤價(元)"]))
+		stockDict_alldata[line["證券代碼"].strip()][6].append(float(line["成交量(千股)"]))
 
 
-
-###############################################################	
-
-	#這些要在for迴圈裡，我改了一些變數名稱
-	#然後運算的方法稍有不同
-	#舉個例子，應該是stockDict["2801"].opening.append(15.00)之類的
-
-	code.append(listdata[0])
-	abbreviation.append(listdata[1])
-	date.append(listdata[2])
-	opening.append(float(listdata[3]))
-	highest.append(float(listdata[4]))
-	lowest.append(float(listdata[5]))
-	closing.append(float(listdata[6]))
-	volume.append(int(listdata[7]))
-###############################################################
-
-
-
-##############################################################
-
-#這些應該在__init__這個function裡面
-
-code = [] #股票代碼(變數名稱不要包含空白)
-abbreviation = [] #公司簡稱
-date = [] #日期, 用datetime物件，所以要import datetime
-opening = [] #開盤價
-highest = [] #最高價
-lowest = [] #最低價
-closing = [] #收盤價
-volume = [] #成交量(千股)
-##############################################################
-
-
+dict = dict()#儲存一家一家的公司，key是股票代碼，value是那間公司的「資料」（所以新建一個「stock」class, 這個class 儲存一家公司的資料）
+for key in stockDict_alldata.keys():
+	dict[key] = stock(key)
 
 filehandler.close()
