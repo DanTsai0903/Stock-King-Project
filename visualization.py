3b8a396f6a6161b939d026b6ca02cbe08f5090df
zoe60440 <46042708+zoe60440@users.noreply.github.com>
2019-01-04 20:51:56 +0800

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -103,11 +103,11 @@ class BigFrame(tk.Frame):
 		self.Btn今日收盤突破季線 = tk.Button(self,text = "今日收盤突破季線",command = self.clickBtn今日收盤突破季線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn今日收盤跌破月線 = tk.Button(self,text = "今日收盤跌破月線",command = self.clickBtn今日收盤跌破月線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn今日收盤跌破季線 = tk.Button(self,text = "今日收盤跌破季線",command = self.clickBtn今日收盤跌破季線,width = 32,font = f,bg = "Antiquewhite")
-		self.Lbl空行 = tk.Label(self,width = 32,font = f)
+		self.Lbl空行 = tk.Label(self,width = 32,font = f,bg = "snow")
 		
 		
-		self.lbl選股條件 = tk.Label(self,text = "選股條件",relief = "ridge",height = 1,width = 32,font = f)
-		self.lbl選股條件line1 = tk.Label(self,anchor = 'w',height = 1,width = 40 ,font = f,background = 'Beige')
+		self.lbl選股條件 = tk.Label(self,text = "選股條件",relief = "ridge",height = 1,width = 32,font = f ,bg = "beige")
+		self.lbl選股條件line1 = tk.Label(self,anchor = 'w',height = 1,width = 40 ,font = f,background = 'snow')
 		self.lbl選股條件line2 = tk.Label(self,anchor = 'w',height = 1,width = 40 ,font = f,background = 'snow')
 		self.lbl選股條件line3 = tk.Label(self,anchor = 'w',height = 1,width = 40 ,font = f,background = 'snow')
 		self.lbl選股條件line4 = tk.Label(self,anchor = 'w',height = 1,width = 40 ,font = f,background = 'snow')
@@ -115,9 +115,9 @@ class BigFrame(tk.Frame):
 		self.lbl選股條件line6 = tk.Label(self,anchor = 'w',height = 1,width = 40 ,font = f,background = 'snow')
 		
 		
-		self.Btn開始選股 = tk.Button(self,text = "開始選股",height = 1,width = 16,font = f,background = 'BurlyWood')
-		self.Btn重設條件 = tk.Button(self,text = "重設條件",command = self.clickDelBtn,height = 1,width = 16,font = f,background = 'BurlyWood')
-		self.BtnQuit = tk.Button(self,text = "Quit!",command = self.quit,height = 1,width = 16,font = f,background = 'BurlyWood')
+		self.Btn開始選股 = tk.Button(self,text = "開始選股",height = 1,width = 16,font = f,background = 'Gold')
+		self.Btn重設條件 = tk.Button(self,text = "重設條件",command = self.clickDelBtn,height = 1,width = 16,font = f,background = 'ivory')
+		self.BtnQuit = tk.Button(self,text = "Quit!",command = self.quit,height = 1,width = 32,font = f,background = 'Ivory')
 	   #設定位置------------------------------------------------------------------------------------
 		self.Btn突破.grid(row = 0,column = 0,columnspan = 3,sticky = tk.NE + tk.SW)
 		self.Btn乖離.grid(row = 0,column = 3,columnspan = 3,sticky = tk.NE + tk.SW)
@@ -142,7 +142,7 @@ class BigFrame(tk.Frame):
 		
 		self.Btn開始選股.grid(row = 17,column = 0,columnspan = 6,sticky = tk.NE + tk.SW)
 		self.Btn重設條件.grid(row = 17,column = 6,columnspan = 6,sticky = tk.NE + tk.SW)
-		self.BtnQuit.grid(row = 18,column = 6,columnspan = 6,sticky = tk.NE + tk.SW)
+		self.BtnQuit.grid(row = 18,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
 
 		self.全選股條件line.append(self.lbl選股條件line1)
 		self.全選股條件line.append(self.lbl選股條件line2)
@@ -168,7 +168,7 @@ class BigFrame(tk.Frame):
 		self.Btn今日收盤突破季線 = tk.Button(self,text = "今日收盤突破季線",command = self.clickBtn今日收盤突破季線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn今日收盤跌破月線 = tk.Button(self,text = "今日收盤跌破月線",command = self.clickBtn今日收盤跌破月線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn今日收盤跌破季線 = tk.Button(self,text = "今日收盤跌破季線",command = self.clickBtn今日收盤跌破季線,width = 32,font = f,bg = "Antiquewhite")
-		self.Lbl空行 = tk.Label(self,width = 32,font = f)
+		self.Lbl空行 = tk.Label(self,width = 32,font = f,bg="snow")
 		
 		self.Btn今日盤中突破5日新高.grid(row = 1,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
 		self.Btn今日收盤突破月線.grid(row = 2,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
@@ -189,7 +189,7 @@ class BigFrame(tk.Frame):
 		self.Btn股價大於周線 = tk.Button(self,text = "股價大於周線5%",command = self.clickBtn股價大於周線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn股價大於月線 = tk.Button(self,text = "股價大於月線10%",command = self.clickBtn股價大於月線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn股價大於季線 = tk.Button(self,text = "股價大於季線20%",command = self.clickBtn股價大於季線,width = 32,font = f,bg = "Antiquewhite")
-		self.Lbl空行 = tk.Label(self,width = 32,font = f)
+		self.Lbl空行 = tk.Label(self,width = 32,font = f,bg="snow")
 		 
 		self.Btn股價大於周線.grid(row = 1,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
 		self.Btn股價大於月線.grid(row = 2,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
@@ -211,7 +211,7 @@ class BigFrame(tk.Frame):
 		self.Btn月線大於季線 = tk.Button(self,text = "月線大於季線",command = self.clickBtn月線大於季線,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn週收盤連兩周上漲 = tk.Button(self,text = "週收盤連兩周上漲",command = self.clickBtn週收盤連兩周上漲,width = 32,font = f,bg = "Antiquewhite")
 		self.Btn月收盤連兩周上漲 = tk.Button(self,text = "月收盤連兩周上漲",command = self.clickBtn月收盤連兩周上漲,width = 32,font = f,bg = "Antiquewhite")
-		self.Lbl空行 = tk.Label(self,width = 32,font = f)
+		self.Lbl空行 = tk.Label(self,width = 32,font = f,bg="snow")
 		
 		self.Btn均線多頭排列.grid(row = 1,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
 		self.Btn均線空頭排列.grid(row = 2,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
@@ -235,7 +235,7 @@ class BigFrame(tk.Frame):
 		self.BtnKD死亡交叉日 = tk.Button(self,text = "KD死亡交叉(日)",command = self.clickBtnKD死亡交叉日,width = 32,font = f,bg = "Antiquewhite")
 		self.BtnKD死亡交叉週 = tk.Button(self,text = "KD死亡交叉(週)",command = self.clickBtnKD死亡交叉週,width = 32,font = f,bg = "Antiquewhite")
 		self.BtnKD死亡交叉月 = tk.Button(self,text = "KD死亡交叉(月)",command = self.clickBtnKD死亡交叉月,width = 32,font = f,bg = "Antiquewhite")
-		self.Lbl空行 = tk.Label(self,width = 32,font = f)
+		self.Lbl空行 = tk.Label(self,width = 32,font = f,bg="snow")
 		
 		self.BtnKD黃金交叉日.grid(row = 1,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
 		self.BtnKD黃金交叉週.grid(row = 2,column = 0,columnspan = 12,sticky = tk.NE + tk.SW)
@@ -302,15 +302,20 @@ class BigFrame(tk.Frame):
 		for item in self.allcompany:
 			if stockDict_stock[item].MA5[-1] <= stostockDict_stock[item].MA20[-1]:
 				self.allcompany.remove(item)
-
 	def clickBtn月線大於季線(self):
 		self.setclick條件Btn("月線大於季線")
 		for item in self.allcompany:
 			if stockDict_stock[item].MA20[-1] <= stostockDict_stock[item].MA60[-1]:
 				self.allcompany.remove(item)
 	def clickBtn週收盤連兩周上漲(self):
+		for item in self.allcompany:
+			if (stockDict_stock[item].closing[-1] <= stostockDict_stock[item].closing[-5]) or (stockDict_stock[item].closing[-6] <= stostockDict_stock[item].closing[-10]):
+				self.allcompany.remove(item)
 		self.setclick條件Btn("週收盤連兩周上漲")
 	def clickBtn月收盤連兩周上漲(self):
+		for item in self.allcompany:
+			if (stockDict_stock[item].closing[-1] <= stostockDict_stock[item].closing[-20]) or (stockDict_stock[item].closing[-21] <= stostockDict_stock[item].closing[-40]):
+				self.allcompany.remove(item)
 		self.setclick條件Btn("月收盤連兩周上漲")
 
 	#運算技術指標的部分--------------------------------------------------------------------------
